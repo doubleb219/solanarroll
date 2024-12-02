df86f39265a1d02366bda6cc74297deb143d0a9f
doubleb219 <brownsbob56@gmail.com>
2024-12-02 10:21:02 -0600

    init repo

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# solanarroll
+# SOLANAROLL
+
+[https://www.solanaroll.com](https://www.solanaroll.com)
+
+
+Solanaroll is a fast, inexpensive dice game with a user-funded pooled treasury. 
+The dice game is provably-fair and has a 1% house edge.
+
+## Dice Game
+
+The dice game uses a commit-reveal scheme: a reveal number hashed with the blockhash of the reveal number save transaction to obtain a random result. 
+[Commit Reveal Scheme](https://medium.com/gitcoin/commit-reveal-scheme-on-ethereum-25d1d1a25428)
+
+![Solanaroll Play](solanaroll-play.png?raw=true "Solanaroll Play")
+
+## Pooled Treasury
+
+Solanaroll allows users to fund the treasury and obtain treasury tokens that can be redeemed.
+Similar to Serum Swap, your token allocation is pro-rated to the current treasury total funds.
+If you deposit to the treasury and the treasury earns more SOL from game plays, your treasury tokens are worth more SOL.
+
+![Solanaroll](solanaroll-main.png?raw=true "Solanaroll Main")
+
+## Play with any token (TODO)
+
+Using Serum Swap, users will be able to swap any token to SOL before a game play, allowing users to play with any token.
+
+contact:
+mtourig3@gmail.com
---
+++ src/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PlayPage } from './pages/PlayPage';
+import { FundPage } from './pages/FundPage';
+import './index.css';
+import { WalletProvider } from "./util/wallet";
+import { useWallet } from "./util/wallet";
+
+import { BrowserRouter, Route, Switch, Redirect, useRouteMatch } from "react-router-dom";
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+          <WalletProvider>
+            <Header/>
+            <PlayPage/>
+          </WalletProvider>
+      </div>
+    );
+  }
+}
+
+class Fund extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+          <WalletProvider>
+            <Header/>
+            <FundPage/>
+          </WalletProvider>
+      </div>
+    );
+  }
+}
+
+export function Header () {
+    const { wallet, conn } = useWallet();
+    return (
+        <div className="container">
+            <nav className="navbar navbar-dark navbar-expand-lg bg-transparent">
+                <div className="navbar-brand">
+                    <a className=" d-inline-block align-top text-sr" href="https://www.solanaroll.com">SOLANA<i>ROLL</i></a>
+                </div>
+                <button className="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarText"
+                        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse nav-menu-sr" id="navbarText">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="sr-nav-text-1 nav-link text-center sr-border-sm" href="/play">Play</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="sr-nav-text-2 nav-link text-center sr-border-sm" href="/fund">Fund</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+// ========================================
+
+ReactDOM.render(
+
+  <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Game} />
+        <Route path="/play" exact component={Game} />
+        <Route path="/fund" exact component={Fund} />
+        <Redirect from="*" to="/" exact />
+      </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
